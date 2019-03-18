e8e0b576f33cf1324d7139f17984648b12abfd9f
Panji Gumelar <panjiggm@gmail.com>
2019-03-18 17:13:02 +0700

    create streams app

---
+++ src/components/streams/StreamEdit.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+const StreamEdit = () => {
+  return ( <div><h4>Stream Edit</h4></div> )
+}
+ 
+export default StreamEdit;
---
+++ src/components/streams/StreamShow.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const StreamShow = () => {
+  return (
+    <div>
+      <h3>Stream Show</h3>
+    </div>
+  )
+}
+
+export default StreamShow
