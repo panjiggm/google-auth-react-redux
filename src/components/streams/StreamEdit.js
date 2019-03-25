34323c55d52bd05962aa33f389cefe91851545ea
Panji Gumelar <panjiggm@gmail.com>
2019-03-25 16:16:24 +0700

    Use reusable form component for editStream component and implement initialValues as a props

--- src/components/streams/StreamEdit.js
+++ src/components/streams/StreamEdit.js
@@ -1,18 +1,28 @@
 import React from 'react'
 import { connect } from 'react-redux'
+import { fetchStream, editStream } from '../../actions'
 
-import { fetchStream } from '../../actions'
+import StreamForm from './StreamForm'
 
 class StreamEdit extends React.Component {
   componentDidMount() {
     this.props.fetchStream(this.props.match.params.id)
   }
 
+  onSubmit = formValues => {
+    console.log(formValues)
+  }
+
   render() {
     if (!this.props.stream) {
       return <div>Loading...</div>
     }
-    return <div>{this.props.stream.title}</div>
+    return (
+      <div>
+        <h3>Edit Stream</h3>
+        <StreamForm initialValues={this.props.stream} onSubmit={this.onSubmit} />
+      </div>
+    )
   }
 }
 
@@ -23,5 +33,5 @@ const mapStateToProps = (state, ownProps) => {
 
 export default connect(
   mapStateToProps,
-  { fetchStream }
+  { fetchStream, editStream }
 )(StreamEdit)
